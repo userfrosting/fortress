01ba1dd098e321db32096f929a577c20acb31cba
alexweissman <alexw@156-56-95-247.dhcp-bl.indiana.edu>
2015-02-09 14:16:46 -0500

    readme

--- example1.php
+++ example1.php
@@ -1,6 +1,5 @@
 <?php
 
-//set_error_handler('logAllErrors');
 require_once("fortress/config-fortress.php");
 
 // Load the request schema
@@ -11,23 +10,15 @@ $rf = new Fortress\HTTPRequestFortress("get", $requestSchema, "index");
 // Remove ajaxMode and csrf_token from the request data
 $rf->removeFields(['ajaxMode', 'csrf_token']);
 
-// Determine whether this is an AJAX request
-$ajax = $rf->getAjaxMode();
-
-// User must be logged in
-//checkLoggedInUser($ajax);
-
 // Sanitize and validate data, halting on errors
 $rf->sanitize();
 $rf->validate();
 
 // Create a new group with the filtered data
 $data = $rf->data();
-if (!createGroup($data)){
+if (!yourFunctionHere($data)){
     $rf->raiseFatalError();
 }
-
-//restore_error_handler();
 
 // If we've made it this far, success!
 $rf->raiseSuccess();
